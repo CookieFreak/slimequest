3a288aa6b9b5af28babb6ed9a6216d1e511d99f5
CookieFreak <sarah.omar.a.a@gmail.com>
2015-01-06 09:28:00 +0400

    Adding Networking
    Camera still needs tweaking

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,7 +7,8 @@ public class CameraScript : MonoBehaviour
 {
 
 	public GameObject character;
-	
+
+
 	void Update () 
 	{
 		gameObject.transform.position = new Vector3 (character.transform.position.x, character.transform.position.y, camera.transform.position.z);
